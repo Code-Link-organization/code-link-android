90239076225f77286d67be3b1d407ee7f1476261
Mohamed Emad <aleslam12345@gmail.com>
2023-08-22 22:20:43 +0300

    create api constants

---
+++ app/src/main/java/com/ieee/codelink/data/remote/ApiConstants.kt
@@ -0,0 +1,9 @@
+package com.ieee.codelink.data.remote
+
+const val BASE_URL = "http://"
+
+const val LOGIN_END_POINT = "login"
+const val LOGOUT_END_POINT = "logout"
+const val SIGNUP_END_POINT = "signup"
+const val SEND_VERIFICATION_CODE = "send-mail"
+const val CHECK_VERIFICATION_CODE = "check-code"
