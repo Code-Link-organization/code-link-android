8549e1a4215f6de89595832f73af2abf1b8f7c33
Mohamed Emad <aleslam12345@gmail.com>
2023-09-15 03:39:03 +0300

    add create team backend

--- app/src/main/java/com/ieee/codelink/ui/teamsScreens/createTeam/CreateTeamFragment.kt
+++ app/src/main/java/com/ieee/codelink/ui/teamsScreens/createTeam/CreateTeamFragment.kt
@@ -2,27 +2,136 @@ package com.ieee.codelink.ui.teamsScreens.createTeam
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.ieee.codelink.R
 import com.ieee.codelink.core.BaseFragment
-import com.ieee.codelink.core.BaseViewModel
+import com.ieee.codelink.core.ResponseState
 import com.ieee.codelink.databinding.FragmentCreateTeamBinding
+import com.ieee.codelink.domain.models.responses.CreateTeamResponse
+import com.ieee.codelink.ui.main.search.SearchViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class CreateTeamFragment :
     BaseFragment<FragmentCreateTeamBinding>(FragmentCreateTeamBinding::inflate) {
-    override val viewModel: BaseViewModel by viewModels()
+    override val viewModel: SearchViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUri()
         setOnClicks()
+        setObserverts()
+    }
+
+    private fun initUri() {
+        viewModel.createTeamImgUri = null
+    }
+
+    private fun setObserverts() {
+        viewModel.createTeanState.awareCollect { state ->
+            createTeamObserver(state)
+        }
+    }
+
+    private fun createTeamObserver(state: ResponseState<CreateTeamResponse>) {
+        stopLoadingAnimation()
+        when (state) {
+            is ResponseState.Empty -> {}
+            is ResponseState.NotAuthorized,
+            is ResponseState.UnKnownError -> {
+                viewModel.createTeanState.value = ResponseState.Empty()
+            }
+
+            is ResponseState.NetworkError -> {
+                showToast(getString(R.string.network_error), false)
+                viewModel.createTeanState.value = ResponseState.Empty()
+            }
+
+            is ResponseState.Error -> {
+                showToast(state.message.toString(), false)
+                viewModel.createTeanState.value = ResponseState.Empty()
+            }
+
+            is ResponseState.Loading -> {
+                startLoadingAnimation()
+            }
+
+            is ResponseState.Success -> {
+                state.data?.let { response ->
+                    lifecycleScope.launch {
+                        goBack()
+                        viewModel.createTeanState.value = ResponseState.Empty()
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private fun goBack() {
+        findNavController().navigateUp()
+    }
+
+    private fun stopLoadingAnimation() {
+        binding.btnCreate.hideLoading()
+    }
+
+    private fun startLoadingAnimation() {
+        binding.btnCreate.showLoading()
     }
 
     private fun setOnClicks() {
         binding.apply {
+            ivCamera.setOnClickListener {
+               openGallery()
+            }
             btnCreate.setOnClickListener {
-                showToast("Team Created",true)
-                findNavController().navigateUp()
+                val name = etName.text.toString()
+                val description = etProject.text.toString()
+                if (canCreateTeam(name, description)) {
+                    lifecycleScope.launch {
+                        viewModel.createTeam(name, description, viewModel.createTeamImgUri)
+                    }
+                } else {
+                    showToast(getString(R.string.completeAll), false)
+                }
             }
         }
     }
+
+    private fun canCreateTeam(name: String, description: String): Boolean =
+        name.isNullOrEmpty().not() &&
+                description.isNullOrEmpty().not()
+
+
+    private fun openGallery() {
+        singlePhotoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    val singlePhotoPicker =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                CoroutineScope(Dispatchers.Main).launch {
+                    viewModel.createTeamImgUri = uri
+
+                    Glide.with(binding.ivTeamImage)
+                        .load(uri)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerInside()
+                        .placeholder(R.drawable.ic_profile)
+                        .error(R.drawable.ic_profile)
+                        .into(binding.ivTeamImage)
+                }
+            }
+        }
 }
